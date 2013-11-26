5662476eda3138661d3e589ac986ef40ed5a50fe
Leon Guan <guanyuliang0706@gmail.com>
2013-11-26 02:42:38 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PictureServer::Application.config.secret_token = 'fc2122d472a92e69ee4e5d65fc16efa6dbad823061aef7a7a81fcf2d0b777e79fa52553fbb7809e17b8289df3485bad7a11b0509e3e24a1712ed0200837cd228'
